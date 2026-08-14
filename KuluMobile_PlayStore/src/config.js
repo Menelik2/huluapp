7d6dd85231cac5097c12c1e9f8d72d2d83a163f3
Kulu Deploy <deploy@kulu.app>
2026-08-14 17:29:32 +0000

    Add Firebase Web Client ID and API URL to mobile config

--- KuluMobile_PlayStore/src/config.js
+++ KuluMobile_PlayStore/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://YOUR-API-DOMAIN.com/api';
-export const FIREBASE_WEB_CLIENT_ID = 'YOUR_FIREBASE_WEB_CLIENT_ID.apps.googleusercontent.com';
+export const API_BASE_URL = 'https://kulu.xo.je/api';
+export const FIREBASE_WEB_CLIENT_ID = '613404183294-rn12glpfvag3cj2dkddl1dno530mrekg.apps.googleusercontent.com';
